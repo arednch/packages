5f98d0eca28669d3b1aa4bb4a26c51135b26c620
Martin Suess <martin.suess@kun.ch>
2023-11-05 19:18:32 +0100

    more clarifications and redirection of actual output

--- phonebook/files/phonebook.sh
+++ phonebook/files/phonebook.sh
@@ -11,19 +11,21 @@ echo '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">'
 echo '<title>AREDN local phonebook update</title>'
 echo '</head>'
 echo '<body>'
+
 echo 'Trying to update the local phonebook:<br>'
 echo '- fetching CSV from AREDN fileserver<br>'
 echo '- updating local phonebook XML for telephones<br>'
 echo '<br>'
-echo 'Note: This is <b>not</b> updating the CSV from the Google Sheet. This is still a separate step.<br>'
+
+echo 'Note: This is <b>not</b> updating the CSV from the Swiss Telephone Book (Google Sheet). Call your administrator if you need it.'
 echo '<br>'
 
-/etc/cron.hourly/fetch_phonebook
+/etc/cron.hourly/fetch_phonebook &> /dev/null
 if [ $? -eq 0 ] 
 then 
-  echo 'Successfully updated phonebook.'
+  echo 'Successfully downloaded and created phonebooks on this router.'
 else 
-  echo 'Unable to update phonebook.'
+  echo 'Unable to download and/or create phonebook on this router.'
 fi
 
 echo '<br>'
